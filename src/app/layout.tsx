bf3bf2f1171b64595cfec0e09c05ecda92e71972
Lukas van Tienen <Lukas@MacBookPro.home>
2026-06-16 17:05:20 +0200

    Add hive app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,10 @@ export const metadata: Metadata = {
   },
   description: "Rustige huishoudplanning voor het hele gezin.",
   applicationName: "TaskHive",
+  icons: {
+    icon: [{ url: "/icons/icon.svg", type: "image/svg+xml" }],
+    apple: [{ url: "/icons/maskable.svg", type: "image/svg+xml" }],
+  },
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
